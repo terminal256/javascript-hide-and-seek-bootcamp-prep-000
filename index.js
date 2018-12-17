74b2ceb4b2d31680b4ef0b4f217d6caf484b17e0
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 22:51:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ function find(dom) {
   // parg is an object reference to a <p> element
 
   // First check that the element has child nodes 
-  if (parg.hasChildNodes()) {
+  if (dom.hasChildNodes()) {
     var children = parg.childNodes;
   
     for (var i = 0; i < children.length; i++) {
-      // do something with each child as children[i]
-      // NOTE: List is live, adding or removing children will change the list
+      find children[i]
+
     }
   } 
 }
