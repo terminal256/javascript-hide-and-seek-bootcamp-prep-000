d12d998d56d2fce24c35e4a3e3cdb27763803821
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 23:25:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+function getFirstSelector(selector){
+  let doc = document.getElementById('app');
+  return doc.querySelector(selector);
+}
+function nestedTarget(){
+  let doc = document.getElementById('app');
+  return doc.querySelector("#nested .target");
+  
+}
+function increaseRankBy(n){
+  let doc = document.getElementById('app');
+  const lis = doc.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    let oldn = parseInt(lis[i].innerHTML, 10);
+    lis[i].innerHTML = (n + oldn).toString();
+  }
+}
+function deepestChild(){
+  let doc = document.getElementById('app');
+  return find(doc.querySelector("#grand-node"));
+}
 
+function find(dom){
+  return findaux(dom, 0)[0];
+}
+function findaux(dom, depth) {
+  // First check that the element has child nodes 
+  if (dom.hasChildNodes()) {
+    let children = dom.children;
+    let maxDepth = depth;
+    let domAnswer = dom;
+    for (let i = 0; i < children.length; i++) {
+      let ans = findaux(children[i], 1 + depth);
+      let domans = ans[0];
+      let depthans = ans[1];
+      if (depthans > maxDepth){
+        domAnswer = domans; 
+        maxDepth = depthans;
+      }
+    }
+    return [domAnswer, maxDepth];
+  } else {return [dom, depth];} 
+}
