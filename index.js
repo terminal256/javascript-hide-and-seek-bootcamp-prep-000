22a04619767a3cfe77086fd7614943f0780d4a1a
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 22:41:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function getFirstSelector(selector){}
 function nestedTarget(){}
 function increaseRankBy(n){
   for (let i = 0; i < lis.length; i++) {
-    let oldn = 
-  lis[i].innerHTML = (i + 1).toString();
+    let oldn = parseInt(lis[i].innerHTML,10);
+  lis[i].innerHTML = (n + oldn).toString();
 }
 }
 function deepestChild(){}
