a0faa1768f6fcf6968cd364f423ca0d5ed18d5e4
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 22:42:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ for (let i = 0; i < lis.length; i++) {
 function getFirstSelector(selector){}
 function nestedTarget(){}
 function increaseRankBy(n){
-  
+  const lis = doc.querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < lis.length; i++) {
     let oldn = parseInt(lis[i].innerHTML, 10);
     lis[i].innerHTML = (n + oldn).toString();
